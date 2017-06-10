6ae8e04755a2234c94649b0ce152150915293b74
danielschung <gettoheaven@yaho.com>
2017-06-09 23:08:24 -0500

    made changes to jQuery animations

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
 $(document).ready(function (main) {
-	$("#hey-im-daniel").css("opacity", 0).fadeTo(2000, 1);
-	$("#social-container").hide().delay(100).fadeIn(2000);
-	$(".text").css("opacity", 0).delay(500).fadeTo(2000, 1);
-	$("h2").css("opacity", 0).delay(500).fadeTo(2000, 1);
-	$("#experience-text1").hide();
+    $("#experience-text1").hide();
     $("#experience-text2").hide();
     $("#experience-text3").hide();
+	$("#hey-im-daniel").css("opacity", 0).fadeTo(1000, 1);
+	$("h2").css("opacity", 0).delay(700).fadeTo(1000, 1);
     $("#experience-container1").on("click", function () {
-        $("#experience-text1").slideToggle();
-        $("#expand1").slideToggle();
+        $("#experience-text1").slideToggle(200);
+        $("#expand1").slideToggle(200);
     });
     $("#experience-container2").on("click", function () {
-        $("#experience-text2").slideToggle();
-        $("#expand2").slideToggle();
+        $("#experience-text2").slideToggle(200);
+        $("#expand2").slideToggle(200);
     });
     $("#experience-container3").on("click", function () {
-        $("#experience-text3").slideToggle();
-        $("#expand3").slideToggle();
+        $("#experience-text3").slideToggle(200);
+        $("#expand3").slideToggle(200);
     });
     
 });
