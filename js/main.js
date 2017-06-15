80f919c3a04e755f3283624865863474804dccac
danielschung <gettoheaven@yaho.com>
2017-06-15 14:34:55 -0500

    updated mobile responsive design

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
-$(window).on("load", function () {
+$(document).ready(function(start) {
+	$(".body-container").hide();
+});
+
+$(window).on("load", function (loaded) {
+	$(".body-container").show();
 	$(".intro-text, .button, .social-container").hide();
 	$(".experience-text1").hide();
 	$(".experience-text2").hide();
 	$(".experience-text3").hide();
+	$(".content-container").hide();
 	$(".intro").width("0%");
-	$(".intro").animate({width:"50%"},700,"swing");
-	$("h1.intro-text").delay(700).fadeIn(700);
+	$(".intro").animate({width:"50%"},500,"swing");
+	$("h1.intro-text").delay(700).fadeIn(300);
 	$(".inner-container").width("0%");
-	$(".inner-container").animate({width:"100%"},1500,"swing");
-	$("p.intro-text").delay(1000).fadeIn(700);
+	$(".inner-container").animate({width:"100%"},1000,"swing");
+	$("p.intro-text").delay(1000).fadeIn(300);
 	$(".button").delay(1300).fadeTo(1000,100, "swing");
-	$(".social-container").delay(1700).fadeTo(500, 1.0, "swing");
+	$(".social-container").delay(1700).fadeTo(500, 1.0, "swing", function(contentLoad) {
+		$(".content-text, .header-content-text").hide();
+		$(".content-container").show().animate({top:"-7px"}, 300, "swing");
+		$(".content-text, .header-content-text").fadeIn(300);
+	});
 
 	/*-------Start Experience Clicking Functions-------*/
 	$(".content-inner-container").first().on("click", function() {
