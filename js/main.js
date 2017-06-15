6b283603693446c63bd575ee4c66c061745d7d6c
danielschung <gettoheaven@yaho.com>
2017-06-15 11:30:16 -0500

    initial push for website 5.1

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
 $(document).ready(function (main) {
-    $("#experience-text1").hide();
-    $("#experience-text2").hide();
-    $("#experience-text3").hide();
-	$("#hey-im-daniel").css("opacity", 0).fadeTo(2000, 1);
-	$(".container").css("opacity", 0).delay(700).fadeTo(2000, 1);
-    $(".footer").css("opacity", 0).delay(700).fadeTo(2000, 1);
-    $("#experience-container1").on("click", function () {
-        $("#experience-text1").slideToggle(400);
-        $("#expand1").slideToggle(400);
-    });
-    $("#experience-container2").on("click", function () {
-        $("#experience-text2").slideToggle(400);
-        $("#expand2").slideToggle(400);
-    });
-    $("#experience-container3").on("click", function () {
-        $("#experience-text3").slideToggle(400);
-        $("#expand3").slideToggle(400);
-    });
-    
+	$(".intro-text, .button, .social-container").hide();
+	$(".experience-text1").hide();
+	$(".experience-text2").hide();
+	$(".experience-text3").hide();
+	$(".intro").width("0%");
+	$(".intro").animate({width:"50%"},700,"swing");
+	$("h1.intro-text").delay(700).fadeIn(700);
+	$(".inner-container").width("0%");
+	$(".inner-container").animate({width:"100%"},1500,"swing");
+	$("p.intro-text").delay(1000).fadeIn(700);
+	$(".button").delay(1300).fadeTo(1000,100, "swing");
+	$(".social-container").delay(1700).fadeTo(500, 1.0, "swing");
+
+	/*-------Start Experience Clicking Functions-------*/
+	$(".content-inner-container").first().on("click", function() {
+		$(".experience-text1").slideToggle(400);
+		$("#expand1").slideToggle(400);
+	});
+	$(".content-inner-container").first().next().on("click", function() {
+		$(".experience-text2").slideToggle(400);
+		$("#expand2").slideToggle(400);
+	});
+	$(".content-inner-container").first().next().next().on("click", function() {
+		$(".experience-text3").slideToggle(400);
+		$("#expand3").slideToggle(400);
+	});
 });
